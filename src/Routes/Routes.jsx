923d53c0afe90feda3ef0184d0b31b24a62f1c0a
kanikanoorsathi <kahinurakter504@gmail.com>
2026-03-19 10:28:14 +0600

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createBrowserRouter } from "react-router";
+// import { RouterProvider } from "react-router/dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
+
