ee100f50d7823b1d08ee011c064330e9fae6dd9c
kanikanoorsathi <kahinurakter504@gmail.com>
2026-03-22 13:07:54 +0600

    last commit

--- src/Pages/BookDetails/BookDetails.jsx
+++ src/Pages/BookDetails/BookDetails.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import { useLoaderData, useParams } from "react-router";
 import { addToStoredDB } from "../../Utility/addToDB";
 
+import { ToastContainer, toast } from 'react-toastify';
+
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const MySwal = withReactContent(Swal);
+
 const BookDetails = () => {
   const { id } = useParams();
   // console.log(id);
@@ -10,23 +17,38 @@ const BookDetails = () => {
   const singleBook = data.find((book) => book.bookId === bookId);
   //console.log(singleBook)
   // console.log(data);
-  const { bookName, image, publisher,review } = singleBook;
+  const { bookName, image, publisher, review } = singleBook;
 
-  const handleMarkRead = id => {
-     // Store with Id
-     // where to store
-     // array or collectin
-     // if book already exist the show a alart
-     // if book exist then push in the collectin or array
+  const handleMarkRead = (id) => {
+    // Store with Id
+    // where to store
+    // array or collectin
+    // if book already exist the show a alart
+    // if book exist then push in the collectin or array
 
-     addToStoredDB(id)
-  }
+    // MySwal.fire({
+    //   title: <p>Hello World</p>,
+    //   didOpen: () => {
+    //     // `MySwal` is a subclass of `Swal` with all the same instance & static methods
+    //     MySwal.showLoading();
+    //   },
+    // }).then(() => {
+    //   return MySwal.fire(<p>Shorthand works too</p>);
+    // });
+
+
+    toast("Wow so easy!")
+
+
+    addToStoredDB(id);
+  };
   return (
     <div className="">
       <div className="card card-side bg-base-200 mx-w-96 shadow-lg p-6 my-24">
         <figure>
-          <img className='max-w-60 bg-base-300 shadow-lg p-8'
-             src={image}
+          <img
+            className="max-w-60 bg-base-300 shadow-lg p-8"
+            src={image}
             alt="Book"
           />
         </figure>
@@ -34,9 +56,18 @@ const BookDetails = () => {
           <h2 className="card-title">{bookName}</h2>
           <p>Book By : {publisher}</p>
           <p>{review}</p>
+
+          <ToastContainer />
+
+
           <div className="card-actions justify-start gap-6">
-            <button onClick={()=>handleMarkRead(id)} className='btn btn-accent'>Mar as Read</button>
-            <button className='btn btn-info'>Add To WishList</button>
+            <button
+              onClick={() => handleMarkRead(id)}
+              className="btn btn-accent"
+            >
+              Mar as Read
+            </button>
+            <button className="btn btn-info">Add To WishList</button>
           </div>
         </div>
       </div>
--- src/Pages/ReadList/ReadList.jsx
+++ src/Pages/ReadList/ReadList.jsx
@@ -5,22 +5,49 @@ import "react-tabs/style/react-tabs.css";
 import { getStoredBook } from "../../Utility/addToDB";
 import Book from "../Book/Book";
 
+
+
 const ReadList = () => {
-    const [readList, setReadList]= useState([])
-    const data = useLoaderData();
-    console.log(data)
-    useEffect(()=>{
-        const storedBookData = getStoredBook();
-        //console.log(storedBookData);
-        const ConvertedStoredBook = storedBookData.map(id => parseInt(id));
-        //console.log(ConvertedStoredBook);
-        const myReadList = data.filter(book => ConvertedStoredBook.includes(book.bookId));
-        // console.log(myReadList)
-         setReadList (myReadList)
-    },[data])
+  const [readList, setReadList] = useState([]);
+  const [sort, setSort] = useState("");
+  const data = useLoaderData();
+  console.log(data);
+  useEffect(() => {
+    const storedBookData = getStoredBook();
+    //console.log(storedBookData);
+    const ConvertedStoredBook = storedBookData.map((id) => parseInt(id));
+    //console.log(ConvertedStoredBook);
+    const myReadList = data.filter((book) =>
+      ConvertedStoredBook.includes(book.bookId));
+    //console.log(myReadList)
+    setReadList(myReadList);
+  }, [data]);
+
+  const handleSort =(type) =>{
+    setSort(type)
+    if(type === "pages"){
+      const sorteByPages =[...readList].sort((a,b)=>a.totalPages - b.totalPages);
+      setReadList(sorteByPages)
+    }
+    if(type === "ratings"){
+      const sorteByrating=[...readList].sort((a,b)=>a.rating - b.rating);
+      setReadList(sorteByrating)
+    }
+  }
   return (
-    <div>
+    <div> 
       <div className="my-16">
+        <details className="dropdown my-8 flex justify-center items-center">
+          <summary className="btn m-1">Sort By : {sort?sort:""}</summary>
+          <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+            <li>
+              <a onClick={()=>handleSort("pages")}>pages</a>
+            </li>
+            <li>
+              <a onClick={()=>handleSort("ratings")}>ratings</a>
+            </li>
+          </ul>
+        </details>
         <Tabs>
           <TabList>
             <Tab>Read Book List</Tab>
@@ -29,11 +56,11 @@ const ReadList = () => {
 
           <TabPanel>
             <h2>Book i read{readList.length}</h2>
-           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-             {
-                readList.map(b=> <Book key={b.bookId} singleBook={b}></Book>)
-            }
-           </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+              {readList.map((b) => (
+                <Book key={b.bookId} singleBook={b}></Book>
+              ))}
+            </div>
           </TabPanel>
           <TabPanel>
             <h2>My Wish List</h2>
